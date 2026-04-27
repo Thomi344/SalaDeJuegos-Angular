e825addcaf068d9525d14a41403d7f41f8530c45
Dylan Thomas Valenzuela <thomasdylan05@hotmail.com>
2026-04-27 00:02:56 -0300

    implementar navbar global y optimizar persistencia de datos github
    Se agregó el Navbar con navegación activa, se implementó un sistema de caché con pipe/tap en el servicio para evitar recargas innecesarias

--- src/app/servicios/github.ts
+++ src/app/servicios/github.ts
@@ -1,15 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { of, tap } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class Github {
   private readonly apiUrl = 'https://api.github.com/users/thomi344';
+  private DatosCache:any = null;
+
   constructor(private http: HttpClient ){}
 
     obtenerDatosGithub(){
-      return this.http.get(this.apiUrl);
-    }
-  
+      if(this.DatosCache){
+        return of( this.DatosCache);
+      }
+      return this.http.get(this.apiUrl).pipe(
+            tap(res => this.DatosCache = res)
+          );
+  }
+  /* 
+  ---
+  .pipe(): Intercepta la respuesta de la API para guardar una copia en el cache.
+  tap(): Permite ejecutar una acción secundaria (guardar en cache) sin modificar la respuesta original.
+  of(): Devuelve un Observable que emite el valor almacenado en cache, permitiendo que los componentes que consumen este servicio sigan funcionando sin cambios, independientemente de si los datos provienen de la API o del cache.
+  ---
+  */
 }
