99a9272159ba5dc5924d6237c55da2c059e80efe
Dylan Thomas Valenzuela <thomasdylan05@hotmail.com>
2026-04-26 22:51:40 -0300

    integración de API GitHub en Quien Soy e implementación de favicon
    Se agregaron los datos dinámicos del alumno, la explicación de la elección del juego y las instrucciones de uso.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./componentes/home/home').then(m=>m.Home)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./componentes/login/login').then(m=>m.Login)
+    },
+    {
+        path: 'registro',
+        loadComponent: () => import('./componentes/registro/registro').then(m=>m.Registro)
+    },
+    {
+        path: 'quiensoy',
+        loadComponent: () => import('./componentes/quiensoy/quiensoy').then(m=>m.Quiensoy)
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+
+];
--- src/app/componentes/quiensoy/quiensoy.ts
+++ src/app/componentes/quiensoy/quiensoy.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Github } from '../../servicios/github';
+import { OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-quiensoy',
   imports: [],
+  standalone: true,
   templateUrl: './quiensoy.html',
   styleUrl: './quiensoy.css',
 })
-export class Quiensoy {}
+export class Quiensoy implements OnInit {
+
+  DatosAlumno:any;
+
+  constructor(private github: Github){}
+
+  ngOnInit(){
+    this.obtenerDatosGithub();
+  }
+
+  obtenerDatosGithub(){
+    this.github.obtenerDatosGithub().subscribe((data:any)=>{
+      this.DatosAlumno = data;
+    })
+  }
+}
